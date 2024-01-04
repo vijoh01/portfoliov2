c06eb782c61f603162b5fdbadca4f240dc5f9f13
Viktor Johansson <76432491+vijoh01@users.noreply.github.com>
2024-01-04 22:31:29 +0100

    SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import React from 'react'
-
+import vijoh from '@/imgs/vijoh-dark.png'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -11,7 +11,7 @@ export const metadata = {
   keywords: 'web development, React.js, portfolio, app development, code, design, viktor johansson, viktor johansson portfolio, viktor johansson developer, viktor johansson programmerare, UX developer',
   ogType: 'Portfolio',
   ogUrl: 'https://www.vijoh.com/',
-  ogImage: 'https://media.licdn.com/dms/image/D4D16AQHCtts7s2mJdA/profile-displaybackgroundimage-shrink_200_800/0/1703591115065?e=2147483647&v=beta&t=ZotIhEcv_K5-ehepZDMMh6MgZgAqXBoYCRUMnbP3efs',
+  ogImage: vijoh,
 };
 
 export default function RootLayout({
@@ -29,7 +29,7 @@ export default function RootLayout({
         <meta property="og:type" content={metadata.ogType} />
         <meta property="og:url" content={metadata.ogUrl} />
         <meta property="og:description" content={metadata.ogDescription} />
-        <meta property="og:image" content={metadata.ogImage} />
+        <meta property="og:image" content={"@/imgs/vijoh-dark.png"} />
         <meta property="og:url" content="https://www.vijoh.com/#contact" />
         <meta property="og:image:width" content="400" />
 
