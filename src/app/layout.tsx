d8b2e776150d1667c24b988292a6f8c5ccc407a1
Viktor Johansson <76432491+vijoh01@users.noreply.github.com>
2024-01-04 22:24:58 +0100

    SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,10 +6,9 @@ import React from 'react'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Portfolio - Viktor Johansson | Experienced Web & App Developer',
+  ogTitle: 'Portfolio - Viktor Johansson | Experienced Web & App Developer',
   ogDescription: 'Code and Design, I Make Ideas Real. Discover my expertise in React.js and web development through my latest projects',
   keywords: 'web development, React.js, portfolio, app development, code, design, viktor johansson, viktor johansson portfolio, viktor johansson developer, viktor johansson programmerare, UX developer',
-  ogTitle: 'Code and Design, I Make Ideas Real.',
   ogType: 'Portfolio',
   ogUrl: 'https://www.vijoh.com/',
   ogImage: 'https://media.licdn.com/dms/image/D4D16AQHCtts7s2mJdA/profile-displaybackgroundimage-shrink_200_800/0/1703591115065?e=2147483647&v=beta&t=ZotIhEcv_K5-ehepZDMMh6MgZgAqXBoYCRUMnbP3efs',
@@ -21,9 +20,9 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" prefix="og: https://ogp.me/ns#">
        <head>
-        <title>{metadata.title}</title>
+        <title>{metadata.ogTitle}</title>
         <meta name="keywords" content={metadata.keywords} />
 
         <meta property="og:title" content={metadata.ogTitle} />
@@ -31,6 +30,7 @@ export default function RootLayout({
         <meta property="og:url" content={metadata.ogUrl} />
         <meta property="og:description" content={metadata.ogDescription} />
         <meta property="og:image" content={metadata.ogImage} />
+        <meta property="og:url" content="https://www.vijoh.com/#contact" />
         <meta property="og:image:width" content="400" />
 
         <script
