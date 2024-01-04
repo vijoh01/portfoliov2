a753240a9fea01e0ed812729f80319fff18586f5
Viktor Johansson <76432491+vijoh01@users.noreply.github.com>
2024-01-05 00:19:58 +0100

    fixed issue

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -21,7 +21,7 @@ const MobileNav = () => {
 
 
     return (
-        <div className="w-full h-12 bg-primary-light filter bg-opacity-70 dark:bg-primary-dark flex justify-evenly items-center fixed bottom-0 sm:hidden">
+        <div className="w-full h-12 z-30 bg-primary-light filter bg-opacity-70 dark:bg-primary-dark flex justify-evenly items-center fixed bottom-0 sm:hidden">
             <button onClick={() => handleClick('about')} className="text-primary-dark dark:text-primary-light" aria-label="About"><PiUser className={active === 'about' ? `fill-[black] dark:fill-[white]` : `fill-[#00000073] dark:fill-[#ffffff73]`} size={35} /></button>
             <button onClick={() => handleClick('knowledge')} className="text-primary-dark dark:text-primary-light" aria-label="Knowledge"><PiRocketLaunch className={active === 'knowledge' ? `fill-[black] dark:fill-[white]` : `fill-[#00000073] dark:fill-[#ffffff73]`} size={35} /></button>
             <button onClick={() => handleClick('projects')} className="text-primary-dark dark:text-primary-light" aria-label="Projects"><PiProjectorScreenChart className={active === 'projects' ? `fill-[black] dark:fill-[white]` : `fill-[#00000073] dark:fill-[#ffffff73]`} size={35} /></button>
