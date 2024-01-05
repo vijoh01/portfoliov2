0e0193b5ec72c5a12c61b5bd37ffbd2436ad36e0
Viktor Johansson <76432491+vijoh01@users.noreply.github.com>
2024-01-05 19:59:14 +0100

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import React from 'react'
 import vijoh from '@/imgs/vijoh-dark.png'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -43,7 +44,9 @@ export default function RootLayout({
           }}
         />
       </head>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>{children}
+      <SpeedInsights />
+      </body>
     </html>
   )
 }
