13f79afa11333cec71cd7e6055ecb3bbf1d3f5ac
Viktor Johansson <76432491+vijoh01@users.noreply.github.com>
2024-01-09 13:05:13 +0100

    meta change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,12 +4,13 @@ import './globals.css'
 import React from 'react'
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Analytics } from '@vercel/analytics/react';
+
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata = {
+export const metadata: any = {
   ogTitle: 'Portfolio - Viktor Johansson | Experienced Web & App Developer',
   ogDescription: 'Welcome to Vijoh, created by Viktor Johansson, your Lund-based Swedish programmer and web & app developer, turns concepts into reality. Immerse yourself in the craftsmanship of Code and Design. Explore the mastery of React.js and web development showcased through my latest projects. Enhance your digital presence with the expertise of a Lund programmer at Vijoh, where innovation meets precision in the heart of Sweden.',
-  keywords: 'viktor johansson', 'web development, React.js, portfolio, app development, code, design, viktor johansson, viktor johansson portfolio, viktor johansson developer, viktor johansson programmerare, UX developer, vijoh, webutvecklare lund, webdeveloper portfolio, viktor johansson lund, viktor johansson developer, viktor johansson programmer, programmer',
+  keywords: 'viktor johansson, web development, React.js, portfolio, app development, code, design, viktor johansson, viktor johansson portfolio, viktor johansson developer, viktor johansson programmerare, UX developer, vijoh, webutvecklare lund, webdeveloper portfolio, viktor johansson lund, viktor johansson developer, viktor johansson programmer, programmer, developer, programmerare',
   ogType: 'website',
   ogImage: 'https://i.postimg.cc/85kcs3Y1/1.png',
 };
@@ -36,7 +37,8 @@ export default function RootLayout({
         <meta property="twitter:image" content={metadata.ogImage}></meta>
         <meta property="twitter:domain" content="vijoh.com"></meta>
         <meta property="twitter:description" content={metadata.ogDescription}></meta>
-        <meta property="og:image" content={metadata.ogImage} />
+        <meta property="og:image" content={metadata.ogImage}/>
+        <meta property="og:image:width" content="300" />
         <meta property="og:url" content="https://www.vijoh.com/"/>
       </head>
       <body className={inter.className}>{children}
