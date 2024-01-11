f011da8b3dcee93f4cc3e1503371395c6dfeadd3
Viktor Johansson <76432491+vijoh01@users.noreply.github.com>
2024-01-11 22:32:22 +0100

    seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,9 +8,9 @@ import { Analytics } from '@vercel/analytics/react';
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: any = {
-  ogTitle: 'Portfolio - Viktor Johansson | Experienced Web & App Developer',
-  ogDescription: 'Welcome to Vijoh, created by Viktor Johansson, your Lund-based Swedish programmer and web & app developer, turns concepts into reality. Immerse yourself in the craftsmanship of Code and Design. Explore the mastery of React.js and web development showcased through my latest projects. Enhance your digital presence with the expertise of a Lund programmer at Vijoh, where innovation meets precision in the heart of Sweden.',
-  keywords: 'viktor johansson, web development, React.js, portfolio, app development, code, design, viktor johansson, viktor johansson portfolio, viktor johansson developer, viktor johansson programmerare, UX developer, vijoh, webutvecklare lund, webdeveloper portfolio, viktor johansson lund, viktor johansson developer, viktor johansson programmer, programmer, developer, programmerare',
+  ogTitle: 'ViJoh Portfolio - Viktor Johansson | Experienced Web & App Developer',
+  ogDescription: 'Welcome to Viktor Johansson`s Web Development Portfolio. As a seasoned web developer with a passion for crafting exceptional digital experiences, I specialize in front-end and back-end development, responsive design, and creating innovative solutions tailored to your needs. Explore my portfolio to discover a diverse range of projects showcasing expertise in HTML5, CSS3, JavaScript, and more. Let`s collaborate to bring your web vision to life. Contact me for professional and creative web development services.',
+  keywords: 'Web development services, Front-end development, Back-end development, Responsive web design, Custom website development, Mobile app development, E-commerce solutions, Website optimization, User interface (UI) design, User experience (UX) design, HTML5/CSS3 development, JavaScript development, Node.js development, Python/Django development, Website maintenance, CMS development, Web security, SEO-friendly websites, Cross-browser compatibility, Website performance optimization, API integration, Web hosting solutions, Social media integration, Web accessibility, Viktor Johansson Web Developer, Viktor Johansson Portfolio, Viktor Johansson Web Design, Viktor Johansson programmerare, Viktor Johansson webbutvecklare, Viktor Johansson linkedin, Viktor Johansson Dev, Viktor Johansson',
   ogType: 'website',
   ogImage: 'https://i.postimg.cc/qvTpkh9r/1.png',
 };
